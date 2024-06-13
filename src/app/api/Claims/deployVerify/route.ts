65e680d713044f5f218f6d16f72e0d2e24883dd5
Elhamuddin <elhamuddin.mahmoodi@gmail.com>
2024-06-13 13:18:27 +0530

    Filtered Pre-Auth cases

--- src/app/api/Claims/deployVerify/route.ts
+++ src/app/api/Claims/deployVerify/route.ts
@@ -11,7 +11,7 @@ router.get(async (req) => {
         success: true,
         message: "Version Update",
         env: process.env.NEXT_PUBLIC_CONFIG,
-        version: 0.002,
+        version: 0.003,
       },
       { status: 200 }
     );
