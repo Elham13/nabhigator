34802a83d24142385acf3943809a1264125dc694
Akshay Vaidya <akshayvaidya@192.168.1.5>
2024-06-24 20:29:19 +0530

    small change in build script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 cd /home/ubuntu/navigator-admin
 npm install
 echo "Installation completed"
-echo "Deployment initiated at this date : $(date)" > /home/ubuntu/navigator-admin/deployment_log.txt
+echo "Deployment initiated at this date :: $(date)" > /home/ubuntu/navigator-admin/deployment_log.txt
