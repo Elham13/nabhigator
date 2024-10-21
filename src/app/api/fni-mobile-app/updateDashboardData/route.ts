76125568b31ffc84909281454c719998d86ced92
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-10-21 22:03:34 +0530

    Add missing emails

--- src/app/api/fni-mobile-app/updateDashboardData/route.ts
+++ src/app/api/fni-mobile-app/updateDashboardData/route.ts
@@ -7,7 +7,7 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import DashboardData from "@/lib/Models/dashboardData";
-import { HydratedDocument } from "mongoose";
+import { HydratedDocument, Types } from "mongoose";
 import {
   CaseDetail,
   EventNames,
@@ -301,7 +301,7 @@ router.post(async (req) => {
         eventRemarks: `${EventNames.INVESTIGATION_SKIPPED_AND_COMPLETING} with remarks ${remarks}`,
       });
 
-      await informInvestigators({ userName, data: dashboardData });
+      await informInvestigators({ userName, data: dashboardData, userId });
 
       data = await dashboardData.save();
       message =
@@ -340,7 +340,7 @@ router.post(async (req) => {
         eventRemarks: EventNames.INVESTIGATION_SKIPPED_AND_RE_ASSIGNING,
       });
 
-      await informInvestigators({ userName, data: dashboardData });
+      await informInvestigators({ userName, data: dashboardData, userId });
 
       data = await dashboardData.save();
       message =
@@ -383,6 +383,7 @@ router.post(async (req) => {
       await informInvestigators({
         userName,
         data: dashboardData,
+        userId,
         isCancel: true,
       });
 
@@ -436,15 +437,20 @@ export async function POST(request: NextRequest, ctx: RequestContext) {
 async function informInvestigators({
   data,
   userName,
+  userId,
   isCancel,
 }: {
   data: IDashboardData;
   userName: string;
+  userId: string;
   isCancel?: boolean;
 }) {
   const claimType = data?.claimType;
   const recipients: string[] = [];
-  const ccRecipients: string[] = ["Sanjay.Kumar16@nivabupa.com"];
+  const ccRecipients: string[] = [
+    "Sanjay.Kumar16@nivabupa.com",
+    "FIallocation@nivabupa.com",
+  ];
   let invName = "";
 
   const investigators: Investigator[] = await ClaimInvestigator.find({
@@ -459,6 +465,20 @@ async function informInvestigators({
       recipients.push(...inv?.email);
     });
   } else throw new Error("No investigator found");
+
+  const userIds = [new Types.ObjectId(userId)];
+
+  if (data?.teamLead)
+    userIds?.push(data?.teamLead as unknown as Types.ObjectId);
+
+  if (data?.clusterManager)
+    userIds?.push(data?.clusterManager as unknown as Types.ObjectId);
+
+  const users: IUser[] = await User.find({ _id: { $in: userIds } });
+
+  if (users && users?.length > 0) {
+    users?.map((u) => ccRecipients?.push(u?.email));
+  }
 
   const html = `<!doctypehtml><html lang=en xmlns:o=urn:schemas-microsoft-com:office:office xmlns:v=urn:schemas-microsoft-com:vml><title></title><meta content="text/html; charset=utf-8"http-equiv=Content-Type><meta content="width=device-width,initial-scale=1"name=viewport><!--[if mso]><xml><o:officedocumentsettings><o:pixelsperinch>96</o:pixelsperinch><o:allowpng></o:officedocumentsettings></xml><![endif]--><!--[if !mso]><!--><!--<![endif]--><style>*{box-sizing:border-box}body{margin:0;padding:0}a[x-apple-data-detectors]{color:inherit!important;text-decoration:inherit!important}#MessageViewBody a{color:inherit;text-decoration:none}p{line-height:inherit}.desktop_hide,.desktop_hide table{mso-hide:all;display:none;max-height:0;overflow:hidden}.image_block img+div{display:none}sub,sup{line-height:0;font-size:75%}@media (max-width:520px){.mobile_hide{display:none}.row-content{width:100%!important}.stack .column{width:100%;display:block}.mobile_hide{min-height:0;max-height:0;max-width:0;overflow:hidden;font-size:0}.desktop_hide,.desktop_hide table{display:table!important;max-height:none!important}}</style><!--[if mso ]><style>sub,sup{font-size:100%!important}sup{mso-text-raise:10%}sub{mso-text-raise:-10%}</style><![endif]--><body class=body style=background-color:#fff;margin:0;padding:0;-webkit-text-size-adjust:none;text-size-adjust:none><table border=0 cellpadding=0 cellspacing=0 class=nl-container role=presentation style=mso-table-lspace:0;mso-table-rspace:0;background-color:#fff width=100%><tr><td><table border=0 cellpadding=0 cellspacing=0 class="row row-1"role=presentation style=mso-table-lspace:0;mso-table-rspace:0 width=100% align=center><tr><td><table border=0 cellpadding=0 cellspacing=0 class="row-content stack"role=presentation style="mso-table-lspace:0;mso-table-rspace:0;color:#000;width:500px;margin:0 auto"width=500 align=center><tr><td class="column column-1"style=mso-table-lspace:0;mso-table-rspace:0;font-weight:400;text-align:left;padding-bottom:5px;padding-top:5px;vertical-align:top;border-top:0;border-right:0;border-bottom:0;border-left:0 width=100%><table border=0 cellpadding=10 cellspacing=0 class="block-1 heading_block"role=presentation style=mso-table-lspace:0;mso-table-rspace:0 width=100%><tr><td class=pad><h3 style="margin:0;color:#7747ff;direction:ltr;font-family:Arial,'Helvetica Neue',Helvetica,sans-serif;font-size:24px;font-weight:700;letter-spacing:normal;line-height:120%;text-align:left;margin-top:0;margin-bottom:0;mso-line-height-alt:28.799999999999997px"><span class=tinyMce-placeholder style=word-break:break-word>Investigation skipped</span></h3></table></table></table><table border=0 cellpadding=0 cellspacing=0 class="row row-2"role=presentation style=mso-table-lspace:0;mso-table-rspace:0;background-color:#fff width=100% align=center><tr><td><table border=0 cellpadding=0 cellspacing=0 class="row-content stack"role=presentation style="mso-table-lspace:0;mso-table-rspace:0;color:#000;background-color:#fff;width:500px;margin:0 auto"width=500 align=center><tr><td class="column column-1"style=mso-table-lspace:0;mso-table-rspace:0;font-weight:400;text-align:left;padding-bottom:5px;padding-top:5px;vertical-align:top;border-top:0;border-right:0;border-bottom:0;border-left:0 width=100%><table border=0 cellpadding=10 cellspacing=0 class="block-1 paragraph_block"role=presentation style=mso-table-lspace:0;mso-table-rspace:0;word-break:break-word width=100%><tr><td class=pad><div style="color:#101112;direction:ltr;font-family:Arial,'Helvetica Neue',Helvetica,sans-serif;font-size:16px;font-weight:400;letter-spacing:0;line-height:120%;text-align:left;mso-line-height-alt:19.2px"><p style=margin:0>Dear ${invName}<br><br>A case with the ${
     claimType === "PreAuth" ? "Pre-Auth" : "Claim"
