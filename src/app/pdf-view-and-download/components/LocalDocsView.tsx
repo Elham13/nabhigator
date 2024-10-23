7deba25c13848e9c0135d44902ad92d6bf55b106
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-10-23 22:26:25 +0530

    Download all documents button add

--- src/app/pdf-view-and-download/components/LocalDocsView.tsx
+++ src/app/pdf-view-and-download/components/LocalDocsView.tsx
@@ -1,14 +1,16 @@
+"use client";
 import React, { Fragment } from "react";
 import { Button, Menu } from "@mantine/core";
 import { CaseDetail, DocumentData } from "@/lib/utils/types/fniDataTypes";
-import { getTasksAndDocs } from "@/lib/helpers";
+import { getSignedUrlHelper, getTasksAndDocs, showError } from "@/lib/helpers";
 
 type PropTypes = {
   caseData: CaseDetail | null;
   claimType?: "PreAuth" | "Reimbursement";
+  claimId: number;
 };
 
-const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
+const LocalDocsView = ({ caseData, claimType, claimId }: PropTypes) => {
   const {
     tasksAndDocs,
     preAuthFindings,
@@ -21,6 +23,228 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
   });
 
   const docs: Record<string, DocumentData[]> = tasksAndDocs?.docs as any;
+
+  const downloadAllDocuments = async () => {
+    const documents: { url: string; name: string }[] = [];
+
+    if (!!docs && Object.keys(docs).length > 0) {
+      Object.keys(docs).map((docKey) => {
+        const doc = docs[docKey];
+        if (!!doc && doc?.length > 0) {
+          for (const el of doc) {
+            if (el?.docUrl && el?.docUrl?.length > 0) {
+              for (let i = 0; i < el?.docUrl?.length; i++) {
+                documents.push({
+                  url: el?.docUrl[i],
+                  name: `${claimId}_${el?.name}_${i + 1}`,
+                });
+              }
+            }
+          }
+        }
+      });
+    }
+
+    if (
+      caseData?.postQARecommendation?.documents &&
+      caseData?.postQARecommendation?.documents?.length > 0
+    ) {
+      for (
+        let i = 0;
+        i < caseData?.postQARecommendation?.documents?.length;
+        i++
+      ) {
+        documents.push({
+          url: caseData?.postQARecommendation?.documents[i],
+          name: `${claimId}_post_qa_upload_${i + 1}`,
+        });
+      }
+    }
+
+    if (claimType === "PreAuth") {
+      if (caseData?.allocationType === "Single") {
+        if (
+          preAuthFindings?.evidenceDocs &&
+          preAuthFindings?.evidenceDocs?.length > 0
+        ) {
+          for (let i = 0; i < preAuthFindings?.evidenceDocs?.length; i++) {
+            documents.push({
+              url: preAuthFindings?.evidenceDocs[i],
+              name: `${claimId}_evidence_${i + 1}`,
+            });
+          }
+        }
+      } else if (caseData?.allocationType === "Dual") {
+        if (
+          preAuthFindings?.evidenceDocs &&
+          preAuthFindings?.evidenceDocs?.length > 0
+        ) {
+          for (let i = 0; i < preAuthFindings?.evidenceDocs?.length; i++) {
+            documents.push({
+              url: preAuthFindings?.evidenceDocs[i],
+              name: `${claimId}_insured_evidence_${i + 1}`,
+            });
+          }
+        }
+        if (
+          preAuthFindingsHospital?.evidenceDocs &&
+          preAuthFindingsHospital?.evidenceDocs?.length > 0
+        ) {
+          for (
+            let i = 0;
+            i < preAuthFindingsHospital?.evidenceDocs?.length;
+            i++
+          ) {
+            documents.push({
+              url: preAuthFindingsHospital?.evidenceDocs[i],
+              name: `${claimId}_hospital_evidence_${i + 1}`,
+            });
+          }
+        }
+      }
+    } else if (claimType === "Reimbursement") {
+      if (caseData?.allocationType === "Single") {
+        if (
+          rmFindings?.recommendation?.evidences &&
+          rmFindings?.recommendation?.evidences?.length > 0
+        ) {
+          for (
+            let i = 0;
+            i < rmFindings?.recommendation?.evidences?.length;
+            i++
+          ) {
+            documents.push({
+              url: rmFindings?.recommendation?.evidences[i],
+              name: `${claimId}_evidence_${i + 1}`,
+            });
+          }
+        }
+
+        if (
+          rmFindings?.["Insured Verification"]?.reasonOfInsuredNotVisit?.proof
+        ) {
+          const proof =
+            rmFindings?.["Insured Verification"]?.reasonOfInsuredNotVisit
+              ?.proof;
+          if (proof?.length > 0) {
+            for (let i = 0; i < proof?.length; i++) {
+              documents.push({
+                url: proof[i],
+                name: `${claimId}_insured_not_visit_proof_${i + 1}`,
+              });
+            }
+          }
+        }
+
+        if (!!rmFindings?.["Insured Verification"]?.prevInsurancePolicyCopy) {
+          documents.push({
+            url: rmFindings?.["Insured Verification"]?.prevInsurancePolicyCopy,
+            name: `${claimId}_prevInsurancePolicyCopy`,
+          });
+        }
+
+        if (
+          !!rmFindings?.["Insured Verification"]
+            ?.anyInsurancePolicyOtherThanNBHI?.documents
+        ) {
+          const tempDocs =
+            rmFindings?.["Insured Verification"]
+              ?.anyInsurancePolicyOtherThanNBHI?.documents;
+          if (tempDocs?.length > 0) {
+            for (let i = 0; i < tempDocs?.length; i++) {
+              documents.push({
+                url: tempDocs[i],
+                name: `${claimId}_prevInsurancePolicyOtherThanNBHI_${i + 1}`,
+              });
+            }
+          }
+        }
+      } else if (caseData?.allocationType === "Dual") {
+        if (
+          rmFindings?.recommendation?.evidences &&
+          rmFindings?.recommendation?.evidences?.length > 0
+        ) {
+          for (
+            let i = 0;
+            i < rmFindings?.recommendation?.evidences?.length;
+            i++
+          ) {
+            documents.push({
+              url: rmFindings?.recommendation?.evidences[i],
+              name: `${claimId}_insured_evidence_${i + 1}`,
+            });
+          }
+        }
+
+        if (
+          rmFindings?.["Insured Verification"]?.reasonOfInsuredNotVisit?.proof
+        ) {
+          const proof =
+            rmFindings?.["Insured Verification"]?.reasonOfInsuredNotVisit
+              ?.proof;
+          if (proof?.length > 0) {
+            for (let i = 0; i < proof?.length; i++) {
+              documents.push({
+                url: proof[i],
+                name: `${claimId}_insured_not_visit_proof_${i + 1}`,
+              });
+            }
+          }
+        }
+
+        if (!!rmFindings?.["Insured Verification"]?.prevInsurancePolicyCopy) {
+          documents.push({
+            url: rmFindings?.["Insured Verification"]?.prevInsurancePolicyCopy,
+            name: `${claimId}_prevInsurancePolicyCopy`,
+          });
+        }
+
+        if (
+          !!rmFindings?.["Insured Verification"]
+            ?.anyInsurancePolicyOtherThanNBHI?.documents
+        ) {
+          const tempDocs =
+            rmFindings?.["Insured Verification"]
+              ?.anyInsurancePolicyOtherThanNBHI?.documents;
+          if (tempDocs?.length > 0) {
+            for (let i = 0; i < tempDocs?.length; i++) {
+              documents.push({
+                url: tempDocs[i],
+                name: `${claimId}_prevInsurancePolicyOtherThanNBHI_${i + 1}`,
+              });
+            }
+          }
+        }
+
+        if (
+          rmFindingsHospital?.recommendation?.evidences &&
+          rmFindingsHospital?.recommendation?.evidences?.length > 0
+        ) {
+          for (
+            let i = 0;
+            i < rmFindingsHospital?.recommendation?.evidences?.length;
+            i++
+          ) {
+            documents.push({
+              url: rmFindingsHospital?.recommendation?.evidences[i],
+              name: `${claimId}_hospital_evidence_${i + 1}`,
+            });
+          }
+        }
+      }
+    }
+
+    for (let i = 0; i < documents?.length; i++) {
+      const a = document.createElement("a");
+      const signedUrl = await getSignedUrlHelper(documents[i]?.url);
+      a.href = signedUrl;
+      a.target = "_blank";
+      a.download = documents[i]?.name;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
 
   return (
     <Menu shadow="md" width={200}>
@@ -57,7 +281,7 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
                               );
                             }}
                           >
-                            Document: {i + 1}
+                            {`${claimId}_${el?.name}_${i + 1}`}
                           </Menu.Item>
                         );
                       })
@@ -83,7 +307,7 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
                     );
                   }}
                 >
-                  Document: {ind + 1}
+                  {`${claimId}_post_qa_upload_${ind + 1}`}
                 </Menu.Item>
               ))}
             </Fragment>
@@ -108,7 +332,7 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
                       );
                     }}
                   >
-                    Evidence {ind + 1}
+                    {`${claimId}_evidence_${ind + 1}`}
                   </Menu.Item>
                 ))}
               </Fragment>
@@ -132,7 +356,7 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
                         );
                       }}
                     >
-                      Evidence {ind + 1}
+                      {`${claimId}_insured_evidence${ind + 1}`}
                     </Menu.Item>
                   ))}
                 </Fragment>
@@ -154,7 +378,7 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
                         );
                       }}
                     >
-                      Evidence {ind + 1}
+                      {`${claimId}_hospital_evidence${ind + 1}`}
                     </Menu.Item>
                   ))}
                 </Fragment>
@@ -180,7 +404,7 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
                       );
                     }}
                   >
-                    Evidence {ind + 1}
+                    {`${claimId}_evidence_${ind + 1}`}
                   </Menu.Item>
                 ))}
               </Fragment>
@@ -204,7 +428,7 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
                         );
                       }}
                     >
-                      Evidence {ind + 1}
+                      {`${claimId}_insured_evidence_${ind + 1}`}
                     </Menu.Item>
                   ))}
                 </Fragment>
@@ -227,7 +451,7 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
                           );
                         }}
                       >
-                        Evidence {ind + 1}
+                        {`${claimId}_hospital_evidence_${ind + 1}`}
                       </Menu.Item>
                     )
                   )}
@@ -236,6 +460,14 @@ const LocalDocsView = ({ caseData, claimType }: PropTypes) => {
             </Fragment>
           ) : null
         ) : null}
+
+        <Button
+          my="lg"
+          className="mx-auto block"
+          onClick={downloadAllDocuments}
+        >
+          Download All
+        </Button>
       </Menu.Dropdown>
     </Menu>
   );
