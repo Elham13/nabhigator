3da53f7685ced092d6c7f2306e7912d8727f754c
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-10-25 14:46:33 +0530

    a

--- src/app/api/Claims/assignToInvestigator/route.ts
+++ src/app/api/Claims/assignToInvestigator/route.ts
@@ -282,10 +282,7 @@ router.post(async (req) => {
 
       inv?.email?.length > 0 &&
         inv?.email?.map(async (mail: string) => {
-          const cc_recipients: string[] = [
-            "FIAllocation@nivabupa.com",
-            "Sanjay.Kumar16@nivabupa.com",
-          ];
+          const cc_recipients: string[] = ["FIAllocation@nivabupa.com"];
           if (dashboardData?.teamLead) {
             const tl = await User.findById(dashboardData?.teamLead);
             if (tl) cc_recipients?.push(tl?.email);
