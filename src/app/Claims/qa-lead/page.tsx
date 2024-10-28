f45129d7d99b90d7c9c61dc641f3fb95ec9a9ebe
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-10-28 12:53:31 +0530

    add page of qa lead

---
+++ src/app/Claims/qa-lead/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageWrapper from "@/components/ClaimsComponents/PageWrapper";
+
+const QALead = () => {
+  return <PageWrapper title="QA Lead">QALead</PageWrapper>;
+};
+
+export default QALead;
