0ad0d613d4e1e49c91aa27dfd3fc62cc22b414b9
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-10-28 12:51:12 +0530

    Report link brought at first

--- src/app/api/Claims/FNIReverseApiCall/route.ts
+++ src/app/api/Claims/FNIReverseApiCall/route.ts
@@ -42,7 +42,7 @@ router.post(async (req) => {
     if (process.env.NEXT_PUBLIC_CONFIG === "PROD")
       url = "https://www.nivabupa.com";
 
-    const EXECUTIVE_SUMMARY = `${recommendation?.text}${url}${recommendation?.link}`;
+    const EXECUTIVE_SUMMARY = `${url}${recommendation?.link}${recommendation?.text}`;
 
     const preparedPayload = {
       ...payload,
