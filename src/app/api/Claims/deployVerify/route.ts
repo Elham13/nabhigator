abad3bc40fbeb4d439cfacca8c43ff9e360b0ccb
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-10-28 17:08:18 +0530

    nps tasks done

--- src/app/api/Claims/deployVerify/route.ts
+++ src/app/api/Claims/deployVerify/route.ts
@@ -11,7 +11,7 @@ router.get(async (req) => {
         success: true,
         message: "Version Update",
         env: process.env.NEXT_PUBLIC_CONFIG,
-        version: "0.118",
+        version: "0.119",
       },
       { status: 200 }
     );
