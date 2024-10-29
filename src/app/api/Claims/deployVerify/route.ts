25e7768efee7aa69720466ea2742d8c8a8efaad0
Elhamuddin <60072659+Elham13@users.noreply.github.com>
2024-10-29 11:58:42 +0530

    mongodb connenction error

--- src/app/api/Claims/deployVerify/route.ts
+++ src/app/api/Claims/deployVerify/route.ts
@@ -11,7 +11,7 @@ router.get(async (req) => {
         success: true,
         message: "Version Update",
         env: process.env.NEXT_PUBLIC_CONFIG,
-        version: 0.042,
+        version: 0.043,
       },
       { status: 200 }
     );
