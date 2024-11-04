969066aaf8b0043ddd359d83deda4cc9a7b00bb7
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-11-04 19:06:13 +0530

    Changes in qa lead

--- src/app/Claims/qa-lead/components/StatusCell.tsx
+++ src/app/Claims/qa-lead/components/StatusCell.tsx
@@ -4,28 +4,48 @@ import { toast } from "react-toastify";
 import axios from "axios";
 import { EndPoints } from "@/lib/utils/types/enums";
 import { showError } from "@/lib/helpers";
-import { Badge, Button, Flex, Grid, Modal, Select, Title } from "@mantine/core";
+import {
+  Badge,
+  Button,
+  Flex,
+  Grid,
+  Modal,
+  Select,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { DateInput } from "@mantine/dates";
 
 type PropTypes = { user: IUser; refetch: () => void };
+
+interface ILeave {
+  fromDate: Date | null;
+  toDate: Date | null;
+  remark: string;
+}
+
+const leaveInitials: ILeave = {
+  fromDate: null,
+  toDate: null,
+  remark: "",
+};
 
 const StatusCell = ({ user, refetch }: PropTypes) => {
   const [loading, setLoading] = useState<boolean>(false);
   const [open, setOpen] = useState<boolean>(false);
-  const [status, setStatus] = useState<string>(() => {
-    return !!user?.status ? user?.status : "";
-  });
+  const [leave, setLeave] = useState<ILeave>(leaveInitials);
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (type: "Activate" | "Inactivate") => {
     setLoading(true);
     try {
       const payload = {
-        action: "updateStatus",
-        status,
+        action: "updateLeaveStatus",
         id: user?._id,
+        payload: { ...leave, type },
       };
       await axios.post<ResponseType<IUser>>(EndPoints.POST_QA_USER, payload);
 
-      toast.success("Claim Type updated");
+      toast.success("Leave Status updated");
       setOpen(false);
       refetch();
     } catch (error: any) {
@@ -42,44 +62,87 @@ const StatusCell = ({ user, refetch }: PropTypes) => {
         className="cursor-pointer"
         onClick={() => setOpen(true)}
       >
-        {user?.status === "Active" ? (
-          <Badge color="blue" className="!block">
-            {user?.status}
+        {user?.leave?.status === "Approved" ? (
+          <Badge color="red" className="!block">
+            Inactive
           </Badge>
         ) : (
-          <Badge color="red" className="!block">
-            {user?.status}
+          <Badge color="blue" className="!block">
+            Active
           </Badge>
         )}
       </div>
 
       {open && (
         <Modal opened={open} onClose={() => setOpen(false)}>
-          <Title order={4}>Change Claim Type</Title>
+          <Title order={4}>
+            {user?.leave?.status === "Approved" ? "Activate" : "De-Activate"}
+          </Title>
 
-          <Grid>
-            <Grid.Col span={12}>
-              <Select
-                label="Status"
-                placeholder="Select Status"
-                data={["Active", "Inactive"]}
-                value={status}
-                onChange={(val) => setStatus(val || "")}
-                required
-                withAsterisk
-              />
-            </Grid.Col>
-            <Grid.Col span={12} my={10}>
-              <Flex justify="flex-end" gap={2}>
-                <Button color="red" onClick={() => setOpen(false)}>
-                  Cancel
-                </Button>
-                <Button loading={loading} onClick={handleSubmit}>
-                  Change
-                </Button>
-              </Flex>
-            </Grid.Col>
-          </Grid>
+          {user?.leave?.status === "Approved" ? (
+            <Button
+              fullWidth
+              my={30}
+              onClick={() => handleSubmit("Activate")}
+              loading={loading}
+            >
+              Activate
+            </Button>
+          ) : (
+            <Grid>
+              <Grid.Col span={{ sm: 12, md: 6 }}>
+                <DateInput
+                  clearable
+                  minDate={new Date()}
+                  value={leave.fromDate}
+                  onChange={(val) =>
+                    setLeave((prev) => ({ ...prev, fromDate: val }))
+                  }
+                  label="From Date"
+                  placeholder="From Date"
+                />
+              </Grid.Col>
+              <Grid.Col span={{ sm: 12, md: 6 }}>
+                <DateInput
+                  clearable
+                  disabled={!leave?.fromDate}
+                  minDate={new Date(leave.fromDate as Date)}
+                  value={leave.toDate}
+                  onChange={(val) =>
+                    setLeave((prev) => ({ ...prev, toDate: val }))
+                  }
+                  label="To Date"
+                  placeholder="To Date"
+                />
+              </Grid.Col>
+              <Grid.Col span={{ sm: 12, md: 12 }}>
+                <TextInput
+                  value={leave.remark}
+                  onChange={(e) =>
+                    setLeave((prev) => ({
+                      ...prev,
+                      remark: e.target.value,
+                    }))
+                  }
+                  label="Remark"
+                  placeholder="Remark"
+                />
+              </Grid.Col>
+              <Grid.Col span={12} my={10}>
+                <Flex justify="flex-end" gap={2}>
+                  <Button color="red" onClick={() => setOpen(false)}>
+                    Cancel
+                  </Button>
+                  <Button
+                    loading={loading}
+                    onClick={() => handleSubmit("Inactivate")}
+                  >
+                    De-Activate
+                  </Button>
+                </Flex>
+              </Grid.Col>
+            </Grid>
+          )}
         </Modal>
       )}
     </Fragment>
