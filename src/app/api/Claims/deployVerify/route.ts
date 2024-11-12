d93438fa2e158b0afa411d68ca644ff82502b14c
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-11-12 20:22:23 +0530

    version

--- src/app/api/Claims/deployVerify/route.ts
+++ src/app/api/Claims/deployVerify/route.ts
@@ -11,7 +11,7 @@ router.get(async (req) => {
         success: true,
         message: "Version Update",
         env: process.env.NEXT_PUBLIC_CONFIG,
-        version: "0.162",
+        version: "0.163",
       },
       { status: 200 }
     );
