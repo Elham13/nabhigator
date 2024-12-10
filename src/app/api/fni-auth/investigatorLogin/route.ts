b16997523fd7d02d198ea7d2ee0fe1f53ebcb407
Elham13 <elhamuddin.mahmoodi.com>
2024-12-10 08:53:01 +0530

    Pre-QC return the case issue resolve

--- src/app/api/fni-auth/investigatorLogin/route.ts
+++ src/app/api/fni-auth/investigatorLogin/route.ts
@@ -7,29 +7,29 @@ import { Investigator } from "@/lib/utils/types/fniDataTypes";
 import ClaimInvestigator from "@/lib/Models/claimInvestigator";
 import { encrypt } from "@/lib/helpers/authHelpers";
 import { Databases } from "@/lib/utils/types/enums";
-import * as crypto from 'crypto';
+import * as crypto from "crypto";
 import { updateInvestigators } from "@/lib/helpers/autoPreQCHelpers";
 
 const router = createEdgeRouter<NextRequest, RequestContext>();
 const key = crypto
-.createHash('sha512')
-.update('secretKey')
-.digest('hex')
-.substring(0, 32);
+  .createHash("sha512")
+  .update("secretKey")
+  .digest("hex")
+  .substring(0, 32);
 const encryptionIV = crypto
-.createHash('sha512')
-.update('secretIV')
-.digest('hex')
-.substring(0, 16);
-let updatedPass:string;
+  .createHash("sha512")
+  .update("secretIV")
+  .digest("hex")
+  .substring(0, 16);
+let updatedPass: string;
 
 router.post(async (req) => {
   const { phone, password } = await req?.json();
   try {
     if (!phone) throw new Error("phone is required");
     if (!password) throw new Error("password is required");
-    if(password){
-      updatedPass =  decryptData(password);
+    if (password) {
+      updatedPass = decryptData(password);
     }
     await connectDB(Databases.FNI);
 
@@ -48,18 +48,22 @@ router.post(async (req) => {
       const remainingTime = Math.ceil(
         (result.blockedUntil - now.valueOf()) / 1000 / 60
       );
-      throw new Error(`Too many failed attempts. Try again in ${remainingTime} minutes.`)
+      throw new Error(
+        `Too many failed attempts. Try again in ${remainingTime} minutes.`
+      );
     }
     if (result?.password !== updatedPass) {
       const MAX_ATTEMPTS = 3;
-      const BLOCK_DURATION = 10 * 60 * 1000; // 10 minutes in milliseconds      
+      const BLOCK_DURATION = 10 * 60 * 1000; // 10 minutes in milliseconds
       result.failedAttempts += 1;
 
       if (result?.failedAttempts >= MAX_ATTEMPTS) {
         result.failedAttempts = 0; // Reset attempts
         result.blockedUntil = new Date(now.getTime() + BLOCK_DURATION); // Block for 10 minutes
         await result?.save();
-      throw new Error("Too many failed attempts. You are blocked for 10 minutes");
+        throw new Error(
+          "Too many failed attempts. You are blocked for 10 minutes"
+        );
       }
 
       await result?.save();
@@ -96,13 +100,13 @@ router.post(async (req) => {
     );
   }
 });
-function decryptData(encryptedData:any) {
-  const buff = Buffer.from(encryptedData, 'base64')
-  const decipher = crypto.createDecipheriv('aes-256-cbc',key, encryptionIV)
+function decryptData(encryptedData: any) {
+  const buff = Buffer.from(encryptedData, "base64");
+  const decipher = crypto.createDecipheriv("aes-256-cbc", key, encryptionIV);
   return (
-    decipher.update(buff.toString('utf8'), 'hex', 'utf8') +
-    decipher.final('utf8')
-  )
+    decipher.update(buff.toString("utf8"), "hex", "utf8") +
+    decipher.final("utf8")
+  );
 }
 export async function POST(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx) as Promise<void>;
