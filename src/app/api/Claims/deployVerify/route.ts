d632dfa929777361b758347c60de31a7f01b0c3d
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-12-17 10:58:36 +0530

    resolved the issue of re-investigation

--- src/app/api/Claims/deployVerify/route.ts
+++ src/app/api/Claims/deployVerify/route.ts
@@ -11,7 +11,7 @@ router.get(async (req) => {
         success: true,
         message: "Version Update",
         env: process.env.NEXT_PUBLIC_CONFIG,
-        version: "0.195",
+        version: "0.196",
       },
       { status: 200 }
     );
