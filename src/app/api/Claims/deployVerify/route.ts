ac116462f68ec1526f5b562465c5b8ad3ee9c1d0
Shantam Sharma <Shantam.Sharma@nivabupa.com>
2024-12-17 10:13:02 +0530

    version chande 95

--- src/app/api/Claims/deployVerify/route.ts
+++ src/app/api/Claims/deployVerify/route.ts
@@ -11,7 +11,7 @@ router.get(async (req) => {
         success: true,
         message: "Version Update",
         env: process.env.NEXT_PUBLIC_CONFIG,
-        version: "0.194",
+        version: "0.195",
       },
       { status: 200 }
     );
