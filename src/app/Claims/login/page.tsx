a993fa4347c0cf0cdf444c146460dcf89a5012ed
Elham13 <elhamuddin.mahmoodi@gmail.com>
2024-12-23 17:15:24 +0530

    build issues resolve

--- src/app/Claims/login/page.tsx
+++ src/app/Claims/login/page.tsx
@@ -169,7 +169,7 @@ const Login = () => {
               onChange={handleChange}
               onBlur={handleBlur}
             />
-           <ReCAPTCHA  class="mt-4"
+           <ReCAPTCHA  className="mt-4"
             sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
             onChange={setCaptcha}
           />
